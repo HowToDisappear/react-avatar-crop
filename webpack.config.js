cf4a44bd6eaeb9752c126fe079f1f72365e412a3
HowToDisappear <60031509+HowToDisappear@users.noreply.github.com>
2024-04-11 11:45:02 +0800

    build: preserve original class names with hash suffix addition

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,9 @@ module.exports = env => {
                         {
                             loader: 'css-loader',
                             options: {
-                                modules: true,
+                                modules: {
+                                    localIdentName: '[name]__[local]--[hash:base64:5]',
+                                },
                             },
                         }
                     ],
