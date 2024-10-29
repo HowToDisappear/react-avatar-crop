0e2701bcc642e16e79b03046b2cd3e1d2d2dcb37
HowToDisappear <60031509+HowToDisappear@users.noreply.github.com>
2024-10-29 12:09:09 +0700

    fix: implement CSSNumericValue fallback for Firefox

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -6,8 +6,20 @@ class Displacement {
     }
 }
 
-const modifyCssLength = (value, operation) => {
-    const numValue = CSSNumericValue.parse(value);
+const modifyCssLength = (cssText, operation) => {
+    let numValue;
+    if (!window?.CSSNumericValue) {
+        // fallback for Firefox
+        const value = cssText.match(/(\d+(\.\d+)?)/g);
+        const unit = cssText.match(/[a-zA-Z]+/g);
+        numValue = {
+            value,
+            unit,
+        };
+    } else {
+        numValue = CSSNumericValue.parse(cssText);
+    }
+
     return operation(numValue.value) + numValue.unit;
 };
 
